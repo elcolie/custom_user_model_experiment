bbc55c68f8f09fa926d170d1c4f6e822f1360e37
Sarit Ritwirune <sarit@elcolie.com>
2020-10-18 00:06:35 +0700

    First commit

---
+++ custom_user_model_experiment/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from custom_user_model_experiment.users.models import User
+from custom_user_model_experiment.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
